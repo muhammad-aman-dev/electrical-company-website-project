d2df5e8b65c2efde46729aefa33a2ffbe764972a
Muhammad Aman <amanmuhammad567@gmail.com>
2025-10-10 16:45:09 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -42,7 +42,6 @@ export default function Home() {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
     nextArrow: <NextArrow />,
