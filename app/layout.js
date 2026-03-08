320e548019197a89cdc0491e4a20d135319de93b
Muhammad Aman <amanmuhammad567@gmail.com>
2026-03-08 22:53:12 +0500

    Integrate Head component for metadata and verification
    Added Head component for SEO and site verification.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Montserrat, Open_Sans } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -15,17 +16,21 @@ const openSans = Open_Sans({
   variable: "--font-open-sans",
 });
 
-export const metadata = {
-  title: "W&C Electrical",
-  description: "Powering Progress with Precision & Safety",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${montserrat.variable} ${openSans.variable}`}
     >
+      <Head>
+        <title>W&C Electrical</title>
+        <meta
+          name="description"
+          content="Powering Progress with Precision & Safety"
+        />
+            <meta name="google-site-verification" content="aMNposdDi4Pp7h8SlRpVHu5FAHHQA_U9P_TaYrlP0VQ" />
+      </Head>
+
       <body className="antialiased w-full sm:w-[86%] sm:ml-[7%] relative font-[family-name:var(--font-open-sans)]">
         <Navbar />
         {children}
