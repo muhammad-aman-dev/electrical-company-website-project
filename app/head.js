29420c086ed29f81227c12a66decd1cf8971386c
Muhammad Aman <amanmuhammad567@gmail.com>
2026-03-08 22:18:43 +0500

    Update head.js

--- app/head.js
+++ app/head.js
@@ -2,11 +2,7 @@
 export default function Head() {
   return (
     <>
-      {/* Google Search Console verification */}
-      <meta
-        name="google-site-verification"
-        content="aMNposdDi4Pp7h8SlRpVHu5FAHHQA_U9P_TaYrlP0VQ"
-      />
+      <meta name="google-site-verification" content="aMNposdDi4Pp7h8SlRpVHu5FAHHQA_U9P_TaYrlP0VQ" />
     </>
   );
 }
