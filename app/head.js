eb398560047d0e9e2bdc6b7dc9f2b106dd912658
Muhammad Aman <amanmuhammad567@gmail.com>
2026-03-08 23:52:37 +0500

    Delete app/head.js

--- app/head.js
+++
@@ -1,8 +0,0 @@
-// app/head.js
-export default function Head() {
-  return (
-    <>
-      <meta name="google-site-verification" content="aMNposdDi4Pp7h8SlRpVHu5FAHHQA_U9P_TaYrlP0VQ" />
-    </>
-  );
-}
